7bfab9d87b8d38cf88e64b8a96b8aaf22a52f1e1
Marco Antonio Miranda <marcoantoniomiranda713@gmail.com>
2026-04-27 20:04:54 -0300

    Consolida repositório gym em projetos

---
+++ gym/gym/wrappers/record_episode_statistics.py
@@ -0,0 +1,90 @@
+"""Wrapper that tracks the cumulative rewards and episode lengths."""
+import time
+from collections import deque
+
+import numpy as np
+
+import gym
+
+
+class RecordEpisodeStatistics(gym.Wrapper):
+    """This wrapper will keep track of cumulative rewards and episode lengths.
+
+    At the end of an episode, the statistics of the episode will be added to ``info``. After the completion
+    of an episode, ``info`` will look like this::
+
+        >>> info = {
+        ...     ...
+        ...     "episode": {
+        ...         "r": "<cumulative reward>",
+        ...         "l": "<episode length>",
+        ...         "t": "<elapsed time since instantiation of wrapper>"
+        ...     },
+        ... }
+
+    Moreover, the most recent rewards and episode lengths are stored in buffers that can be accessed via
+    :attr:`wrapped_env.return_queue` and :attr:`wrapped_env.length_queue` respectively.
+
+    Attributes:
+        return_queue: The cumulative rewards of the last ``deque_size``-many episodes
+        length_queue: The lengths of the last ``deque_size``-many episodes
+    """
+
+    def __init__(self, env: gym.Env, deque_size: int = 100):
+        """This wrapper will keep track of cumulative rewards and episode lengths.
+
+        Args:
+            env (Env): The environment to apply the wrapper
+            deque_size: The size of the buffers :attr:`return_queue` and :attr:`length_queue`
+        """
+        super().__init__(env)
+        self.num_envs = getattr(env, "num_envs", 1)
+        self.t0 = time.perf_counter()
+        self.episode_count = 0
+        self.episode_returns = None
+        self.episode_lengths = None
+        self.return_queue = deque(maxlen=deque_size)
+        self.length_queue = deque(maxlen=deque_size)
+        self.is_vector_env = getattr(env, "is_vector_env", False)
+
+    def reset(self, **kwargs):
+        """Resets the environment using kwargs and resets the episode returns and lengths."""
+        observations = super().reset(**kwargs)
+        self.episode_returns = np.zeros(self.num_envs, dtype=np.float32)
+        self.episode_lengths = np.zeros(self.num_envs, dtype=np.int32)
+        return observations
+
+    def step(self, action):
+        """Steps through the environment, recording the episode statistics."""
+        observations, rewards, dones, infos = super().step(action)
+        self.episode_returns += rewards
+        self.episode_lengths += 1
+        if not self.is_vector_env:
+            infos = [infos]
+            dones = [dones]
+        else:
+            infos = list(infos)  # Convert infos to mutable type
+        for i in range(len(dones)):
+            if dones[i]:
+                infos[i] = infos[i].copy()
+                episode_return = self.episode_returns[i]
+                episode_length = self.episode_lengths[i]
+                episode_info = {
+                    "r": episode_return,
+                    "l": episode_length,
+                    "t": round(time.perf_counter() - self.t0, 6),
+                }
+                infos[i]["episode"] = episode_info
+                self.return_queue.append(episode_return)
+                self.length_queue.append(episode_length)
+                self.episode_count += 1
+                self.episode_returns[i] = 0
+                self.episode_lengths[i] = 0
+        if self.is_vector_env:
+            infos = tuple(infos)
+        return (
+            observations,
+            rewards,
+            dones if self.is_vector_env else dones[0],
+            infos if self.is_vector_env else infos[0],
+        )
