c3f23662e1a467438373ab6675387fe122c67528
Marco Antonio Miranda <marcoantoniomiranda713@gmail.com>
2026-04-27 19:17:59 -0300

    Consolida repositório automatizando-tarefas em projetos

---
+++ automatizando-tarefas/Execução automatizada do Módulo de voto do sistema GURI da Unipampa/scriptExecucaoFinalRP.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python 
+ 
+import threading
+import time
+import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+def executa(tipoUsuario):
+	if tipoUsuario == "Moderador":
+		driver = webdriver.Chrome()
+		driver.get("http://localhost/sistema/sistemaVoto/")
+		driver.set_window_size(808, 886)
+		time.sleep(5)
+		driver.find_element_by_xpath("//*[@id='div_login']/input[1]").send_keys("renaro")
+		driver.find_element_by_id('login').click()
+		#time.sleep(10)
+		driver.get("http://localhost/sistema/sistemaVoto/reunioes")
+		time.sleep(5)
+		driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/table/tbody/tr[2]/td[9]/input').click()
+		time.sleep(10)
+		driver.find_element_by_xpath('//*[@id="vtab1"]/div[3]/form/div[3]/button').click()
+		time.sleep(10)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		driver.find_element_by_xpath('/html/body/div[2]/form/input[3]').click()
+		time.sleep(10)
+		driver.close()
+		
+	else:
+		driver = webdriver.Chrome()
+		driver.get("http://localhost/sistema/sistemaVoto/")	
+		driver.set_window_size(808, 886)
+		time.sleep(10)
+		driver.find_element_by_xpath("//*[@id='div_login']/input[1]").send_keys("rafael")
+		driver.find_element_by_id('login').click()
+		time.sleep(3)
+		driver.get("http://localhost/sistema/sistemaVoto/reunioes")	
+		time.sleep(3)
+		driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/table/tbody/tr/td[9]/input").click()
+		time.sleep(10)
+		alert = driver.switch_to_alert()
+		alert.accept()
+		time.sleep(2)
+		driver.find_element_by_xpath("//input[@value='Abstencao']").click()
+		time.sleep(2)
+		driver.find_element_by_id('bt_votar').click()
+		time.sleep(2)
+		alert = driver.switch_to_alert()
+		alert.accept()
+		time.sleep(10)
+
+
+
+		driver.close()
+		
+
+
+t = threading.Thread(target=executa,args=("Moderador",))
+t.start()
+
+b = threading.Thread(target=executa,args=("mod",))
+b.start()
---
+++ automatizando-tarefas/README.md
@@ -0,0 +1,11 @@
+# automatizando-tarefas
+
+Estes scripts foram feitos visando automatizar tarefas chatas do meu dia-a-dia, ou de muitos outros ;)
+> Não, eu não tenho preguiça, é apenas uma leve falta de disposição.
+
+
+:shipit:
+
+[Enviar mensagens automatizadas por WhatsApp para múltiplas pessoas sem ter contantos salvos](https://github.com/LuizFritsch/automatizando-tarefas/tree/master/WhatsApp%20Message%20Sender)
+
+[Renovar o empréstimo de seus livros na Unipampa automáticamente](https://github.com/LuizFritsch/automatizando-tarefas/tree/master/Renovacao%20de%20Livros%20Da%20biblioteca%20da%20unipampa)
